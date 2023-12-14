32d3211acdfc97d613d1d742fe5e102a8ded0921
Sebastian Borza <sebito91@gmail.com>
2023-12-14 00:13:15 -0600

    feat(auth): initial tests added

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKeyWithEmptyHeader(t *testing.T) {
+	// initialize empty header
+	header := http.Header{}
+
+	_, err := GetAPIKey(header)
+	if err == nil {
+		t.Fatal("expected an error due to empty header but didn't get one")
+	}
+}
+
+func TestGetAPIKeyWithBadAuthorizationMarker(t *testing.T) {
+	header := http.Header{}
+	header.Add("Authorization", "GarbageKey 12345")
+
+	_, err := GetAPIKey(header)
+	if err == nil {
+		t.Fatal("expected an error due to incorrect Authorization header but didn't get one")
+	}
+}
+
+func TestGetAPIKeyWithValidAPIKey(t *testing.T) {
+	header := http.Header{}
+	header.Add("Authorization", "ApiKey 12345")
+
+	key, err := GetAPIKey(header)
+	if err != nil {
+		t.Fatalf("did not expect error with valid API Key, got: %s", err)
+	}
+
+	if key != "12345" {
+		t.Fatalf("incorrect APIKey retrieved, expected: 12345, got: %s", key)
+	}
+}
